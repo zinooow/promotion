42a0ad2e1471da18b904dd5cddfad749d44ff916
jinho <jihno0547@naver.com>
2025-07-18 14:22:04 +0900

    innit

---
+++ discovery-service/src/main/kotlin/jinho/han/discoveryservice/DiscoveryServiceApplication.kt
@@ -0,0 +1,14 @@
+package jinho.han.discoveryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class DiscoveryServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiscoveryServiceApplication>(*args)
+}
---
+++ discovery-service/src/test/kotlin/jinho/han/discoveryservice/DiscoveryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package jinho.han.discoveryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
