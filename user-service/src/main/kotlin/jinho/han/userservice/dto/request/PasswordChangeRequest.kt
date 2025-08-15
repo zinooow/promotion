d36477054c669b620a40cb18858d4ad784c18c00
jinho <jinho0547@naver.com>
2025-08-15 15:42:53 +0900

    Add: Password 패턴 검증 추가

--- user-service/src/main/kotlin/jinho/han/userservice/dto/request/PasswordChangeRequest.kt
+++ user-service/src/main/kotlin/jinho/han/userservice/dto/request/PasswordChangeRequest.kt
@@ -1,6 +1,7 @@
 package jinho.han.userservice.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 class PasswordChangeRequest(
     val id: Int,
@@ -9,5 +10,7 @@ class PasswordChangeRequest(
     val currentPassword: String,
 
     @field:NotBlank("Password is required")
+    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+        message = "Password must be at least 8 characters long and contain both letters and numbers")
     val newPassword: String
 )
--- user-service/src/main/kotlin/jinho/han/userservice/dto/request/SignupRequest.kt
+++ user-service/src/main/kotlin/jinho/han/userservice/dto/request/SignupRequest.kt
@@ -2,6 +2,7 @@ package jinho.han.userservice.dto.request
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
 data class SignupRequest(
     @field:NotBlank("Email is required")
@@ -9,6 +10,8 @@ data class SignupRequest(
     val email: String,
 
     @field:NotBlank("Password is required")
+    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+        message = "Password must be at least 8 characters long and contain both letters and numbers")
     val password: String,
 
     @field:NotBlank("Name is required")
